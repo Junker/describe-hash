20dbbbea630055b2401f13a55fbb21216960dc46
Junker <1144095+Junker@users.noreply.github.com>
2020-07-18 22:56:14 +0700

    add license

--- describe-hash.el
+++ describe-hash.el
@@ -4,6 +4,18 @@
 ;; URL: https://github.com/Junker/describe-hash
 ;; Original: https://www.emacswiki.org/emacs/HashMap
 
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; This package provides help function for examining hash map values,
